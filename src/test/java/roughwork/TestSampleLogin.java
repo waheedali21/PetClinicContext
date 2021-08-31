4de6808ca905337eb97b2f16bdb43fa6619b5151
waheedali21 <waheedspc1@gmail.com>
2021-08-31 17:09:16 +0400

    new rough test class

---
+++ src/test/java/roughwork/TestSampleLogin.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package roughwork;
+
+/**
+ * @author h
+ *
+ */
+public class TestSampleLogin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This new test class for rough work");
+	}
+
+}
